df0167a1dc1ba31f1e834b70dd9fd41718e4fc9c
Ryan Dens <ryan.dens@contrastsecurity.com>
2018-02-07 21:20:18 -0500

    replaced old color skill with new color skill

--- Color_Picker/lambda/custom/__init__.py
+++
@@ -1,5 +0,0 @@
-from .main import lambda_handler
-from .events import on_session_started, on_launch, on_intent, on_session_ended
-from .helpers import build_speechlet_response, build_response
-from .skill_behavior import get_welcome_response, handle_session_end_request, \
-    create_favorite_color_attributes, set_color_in_session, get_color_from_session
--- test/test_helpers.py
+++ test/test_helpers.py
@@ -7,12 +7,7 @@ lambda_functions = importlib.import_module('Color_Picker.lambda.custom')
 class TestBasic(unittest.TestCase):
 
     def setUp(self):
-        pass
-
-    def test_build_speechlet_response(self):
-        response = lambda_functions.helpers.build_speechlet_response('title', 'output', 'reprompt_text', 'should_end_session')
-
-        expected_response = {
+        self.expected_speechlet_response = {
             'outputSpeech': {
                 'type': 'PlainText',
                 'text': 'output'
@@ -31,4 +26,10 @@ class TestBasic(unittest.TestCase):
             'shouldEndSession': 'should_end_session'
         }
 
-        self.assertEqual(response, expected_response)
+    def test_build_speechlet_response(self):
+        response = lambda_functions.helpers.build_speechlet_response('title', 'output', 'reprompt_text', 'should_end_session')
+
+        self.assertEqual(response, self.expected_speechlet_response)
+
+    def test_build_response(self):
+        pass
