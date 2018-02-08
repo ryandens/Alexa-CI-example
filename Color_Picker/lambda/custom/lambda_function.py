4ca5fd0af2a969603d08e755a24b3f1e6cf5b793
Ryan Dens <ryan.dens@contrastsecurity.com>
2018-02-07 19:54:20 -0500

    moved core logic from /src to /lambda/custom

--- Color_Picker/lambda/custom/lambda_function.py
+++ Color_Picker/lambda/custom/lambda_function.py
@@ -11,16 +11,16 @@ from __future__ import print_function
 
 
 # --------------- Helpers that build all of the responses ----------------------
-from Color_Picker.src.helpers import build_speechlet_response, build_response
+from .helpers import build_speechlet_response, build_response
 
 
 # --------------- Functions that control the skill's behavior ------------------
-from Color_Picker.src.skill_behavior import get_welcome_response, handle_session_end_request, \
+from .skill_behavior import get_welcome_response, handle_session_end_request, \
     create_favorite_color_attributes, set_color_in_session, get_color_from_session
 
 # --------------- Events ------------------
-from Color_Picker.src.events import on_session_started, on_launch, on_intent, on_session_ended
+from .events import on_session_started, on_launch, on_intent, on_session_ended
 
 # --------------- Main handler ------------------
 
-from Color_Picker.src.main import lambda_handler
+from .main import lambda_handler
