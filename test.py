ec58445e66d82ad37f5db88a025f585d6ff1d907
Ryan Dens <ryan.dens@contrastsecurity.com>
2018-02-15 17:15:51 -0500

    selenium test

---
+++ test.py
@@ -0,0 +1,6 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.add_argument('-headless')
+firefox = Firefox(firefox_options=options)
